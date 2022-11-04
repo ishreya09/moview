5b2b5f54456e9a8e56e1c1a931f9be7aec0a9423
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2022-11-04 18:12:16 +0530

    Install Bootstrap and add navbar and footer for app

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+    return (
+        <div className="Footer bg-primary">
+            <span className="text-light">&copy; Copyright 2022</span>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/Navbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div className="NavBar" >
+            <div className="container-fluid bg-primary">
+                <nav className="navbar navbar-expand-lg  text-light">
+
+                    <a className="navbar-brand text-light" href="/">Moview</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <button className="btn btn-outline-primary">
+                                    <a className="nav-link text-light" aria-current="page" href="#">Home</a>
+                                </button>
+                            </li>
+                            <li className="nav-item">
+                                <button className="btn btn-outline-primary">
+                                    <a className="nav-link text-light" href="#">Link</a>
+                                </button>
+                            </li>
+                        </ul>
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-dark text-light" type="submit">Search</button>
+                        </form>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
