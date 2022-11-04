f45327ed805ffc4f8eee54a271fb37ff509fadf0
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2022-11-05 00:10:43 +0530

    add card layout and make a movie list format

---
+++ src/Card.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+
+function Card(props) {
+    return (
+        <div className="MyCard">
+            <div className="card" style={{width: "18rem"}}>
+                <img src={props.imageUrl} className="CardImg card-img-top" alt="img" />
+                    <div className="card-body">
+                        <h5 className="card-title">{props.title}</h5>
+                        <p className="card-text">{props.metades}</p>
+                        <a href={props.link} className="Button btn btn-primary">Review Movie</a>
+                    </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default Card;
---
+++ src/MovieList.js
@@ -0,0 +1,56 @@
+import React from "react"
+import Card from "./Card";
+
+function MovieList(){
+    return (
+        // use map to parse the whole Cards from database
+        <div className="container">
+        <Card imageUrl="https://images.pexels.com/photos/14270535/pexels-photo-14270535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+        title="Inception" metades="Some quick example text to build on the card title and make up the bulk of the card's content." 
+        link="#" />
+        <Card imageUrl="https://images.pexels.com/photos/14270535/pexels-photo-14270535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+        title="Inception" metades="Some quick example text to build on the card title and make up the bulk of the card's content." 
+        link="#" />
+        <Card imageUrl="https://images.pexels.com/photos/14270535/pexels-photo-14270535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+        title="Inception" metades="Some quick example text to build on the card title and make up the bulk of the card's content." 
+        link="#" />
+        <Card imageUrl="https://images.pexels.com/photos/14270535/pexels-photo-14270535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+        title="Inception" metades="Some quick example text to build on the card title and make up the bulk of the card's content." 
+        link="#" />
+        <Card imageUrl="https://images.pexels.com/photos/14270535/pexels-photo-14270535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+        title="Inception" metades="Some quick example text to build on the card title and make up the bulk of the card's content." 
+        link="#" />
+        <Card imageUrl="https://images.pexels.com/photos/14270535/pexels-photo-14270535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+        title="Inception" metades="Some quick example text to build on the card title and make up the bulk of the card's content." 
+        link="#" />
+        <Card imageUrl="https://images.pexels.com/photos/14270535/pexels-photo-14270535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+        title="Inception" metades="Some quick example text to build on the card title and make up the bulk of the card's content." 
+        link="#" />
+        <Card imageUrl="https://images.pexels.com/photos/14270535/pexels-photo-14270535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+        title="Inception" metades="Some quick example text to build on the card title and make up the bulk of the card's content." 
+        link="#" />
+        <Card imageUrl="https://images.pexels.com/photos/14270535/pexels-photo-14270535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+        title="Inception" metades="Some quick example text to build on the card title and make up the bulk of the card's content." 
+        link="#" />
+        <Card imageUrl="https://images.pexels.com/photos/14270535/pexels-photo-14270535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+        title="Inception" metades="Some quick example text to build on the card title and make up the bulk of the card's content." 
+        link="#" />
+        <Card imageUrl="https://images.pexels.com/photos/14270535/pexels-photo-14270535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+        title="Inception" metades="Some quick example text to build on the card title and make up the bulk of the card's content." 
+        link="#" />
+        <Card imageUrl="https://images.pexels.com/photos/14270535/pexels-photo-14270535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+        title="Inception" metades="Some quick example text to build on the card title and make up the bulk of the card's content." 
+        link="#" />
+        <Card imageUrl="https://images.pexels.com/photos/14270535/pexels-photo-14270535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+        title="Inception" metades="Some quick example text to build on the card title and make up the bulk of the card's content." 
+        link="#" />
+        <Card imageUrl="https://images.pexels.com/photos/14270535/pexels-photo-14270535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+        title="Inception" metades="Some quick example text to build on the card title and make up the bulk of the card's content." 
+        link="#" />
+        
+        </div>
+
+    );
+}
+
+export default MovieList;
