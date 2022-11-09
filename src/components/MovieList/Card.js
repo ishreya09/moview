03ce984bb82f1e70fc05bacdcc0e7172b2f59724
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2022-11-10 00:07:40 +0530

    add href to image in movie card

--- src/components/MovieList/Card.js
+++ src/components/MovieList/Card.js
@@ -8,13 +8,15 @@ import "./MovieList.css"
 function Card(props) {
     return (
         <div className="MyCard">
-            <div className="card" style={{width: "18rem"}}>
-                <img src={props.imageUrl} className="CardImg card-img-top" alt="img" />
-                    <div className="card-body">
-                        <h5 className="card-title">{props.title}</h5>
-                        <p className="card-text">{props.metades}</p>
-                        <a href="/movie/123" className="Button btn btn-primary">Review Movie</a>
-                    </div>
+            <div className="card" style={{ width: "18rem" }}>
+                <a href="/movie/123" className="">
+                    <img src={props.imageUrl} className="CardImg card-img-top" alt="img" />
+                </a>
+                <div className="card-body">
+                    <h5 className="card-title">{props.title}</h5>
+                    <p className="card-text">{props.metades}</p>
+                    <a href="/movie/123" className="Button btn btn-primary">Review Movie</a>
+                </div>
             </div>
         </div>
     );
