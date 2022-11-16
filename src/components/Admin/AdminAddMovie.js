963a9b0d37dec203bea634ec78cab8f7a2b516dc
Shreeja R <shreejar197@gmail.com>
2022-11-16 21:22:48 +0530

    some changes made to admin add movie page

--- src/components/Admin/AdminAddMovie.js
+++ src/components/Admin/AdminAddMovie.js
@@ -11,8 +11,8 @@ class AddMovieForm extends React.Component {
             image: "",
             trailer: "",
             summary: "",
-            cast: "",
-            reviewArray: "",
+            cast: [{}],
+            reviewsArray: [{}],
             genre: "",
             language:"",
             rating:0,
@@ -21,8 +21,8 @@ class AddMovieForm extends React.Component {
             inTheatres:"",
             BookMyShow:"",
             releaseDate:"",
-            platforms:"",
-            category:"",
+            platforms:[{}],
+            category:[{}],
 
         };
         // bind all the events here
@@ -31,10 +31,17 @@ class AddMovieForm extends React.Component {
         this.changeImage = this.changeImage.bind(this);
         this.changeTrailer = this.changeTrailer.bind(this);
         this.changeSummary = this.changeSummary.bind(this);
+        this.changeCast = this.changeCast.bind(this);
         this.changeLanguage = this.changeLanguage.bind(this);
+        this.changeGenre = this.changeGenre.bind(this);
         this.changeRating = this.changeRating.bind(this);
+        this.changeDuration = this.changeDuration.bind(this);
+        this.changeCensorBoardRating = this.changeCensorBoardRating.bind(this);
+        this.changePlatforms = this.changePlatforms.bind(this);
+        this.changeCategory = this.changeCategory.bind(this);
         this.changeInTheatres = this.changeInTheatres.bind(this);
         this.changeBookMyShow = this.changeBookMyShow.bind(this);
+        this.changeReviewsArray = this.changeReviewsArray.bind(this);
         this.changeReleaseDate = this.changeReleaseDate.bind(this);
         this.onAddMovie = this.onAddMovie.bind(this);
     }
@@ -54,8 +61,26 @@ class AddMovieForm extends React.Component {
     changeSummary = (event) => {
         this.setState({ summary: event.target.value });
     }
+    changeCast = (event) => {
+        this.setState({ cast: event.target.value });
+    }
     changeLanguage = (event) => {
         this.setState({ language: event.target.value });
+    }
+    changeGenre = (event) => {
+        this.setState({ genre: event.target.value });
+    }
+    changeDuration = (event) => {
+        this.setState({ duration: event.target.value });
+    }
+    changeCensorBoardRating = (event) => {
+        this.setState({ censorBoardRating: event.target.value });
+    }
+    changePlatforms = (event) => {
+        this.setState({ platforms: event.target.value });
+    }
+    changeCategory = (event) => {
+        this.setState({ category: event.target.value });
     }
     changeRating = (event) => {
         this.setState({ rating: event.target.value });
@@ -72,6 +97,7 @@ class AddMovieForm extends React.Component {
 
 
     onAddMovie = (event) => {
+        
         event.preventDefault();
         const addMovieAdmin = {
             title: this.state.title,
@@ -80,10 +106,14 @@ class AddMovieForm extends React.Component {
             trailer: this.state.trailer,
             summary: this.state.summary,
             language: this.state.language,
+            genre: this.state.genre,
+            duration: this.state.duration,
+            censorBoardRating: this.state.censorBoardRating,
             rating: this.state.rating,
             inTheatres: this.state.inTheatres,
             BookMyShow: this.state.BookMyShow,
             releaseDate: this.state.releaseDate,
+            
 
         }
         axios.post("http://localhost:4000/admin", addMovieAdmin)
@@ -103,31 +133,78 @@ class AddMovieForm extends React.Component {
     render() {
         return (
             <div className="AddMovieForm container-fluid">
-                <h1>Sign Up</h1>
+                <h1>Add Movie</h1>
                 <form method="post" onSubmit={this.onAddMovie}>
                     <div className="mb-3 text-start">
                         <label for="title" className="form-label text-muted ">Title</label>
-                        <input required type="text" name="title" onChange={this.changeTitle} value={this.state.username} className="form-control" id="username" placeholder="Username" />
+                        <input required type="text" name="title" onChange={this.changeTitle} value={this.state.title} className="form-control" id="title" placeholder="Movie Title" />
                     </div>
                     <div className="mb-3 text-start">
-                        <label for="name" className="form-label text-muted ">Name</label>
-                        <input required type="text" name="name" value={this.state.name} onChange={this.changeName} className="form-control" id="name" placeholder="Full Name" />
+                        <label for="metadescription" className="form-label text-muted ">Metadescription</label>
+                        <input required type="text" name="metadescription" onChange={this.changeMetadescription} value={this.state.metadescription} className="form-control" id="metadescription" placeholder="Movie Metadescription" />
                     </div>
                     <div className="mb-3 text-start">
-                        <label for="username" className="form-label text-muted ">Email</label>
-                        <input required type="email" name="email" value={this.state.email} onChange={this.changeEmail} className="form-control" id="email" placeholder="Email" />
+                        <label for="image" className="form-label text-muted ">Image</label>
+                        <input required type="text" name="title" onChange={this.changeImage} value={this.state.image} className="form-control" id="image" placeholder="Movie Image" />
                     </div>
                     <div className="mb-3 text-start">
-                        <label for="password" className="form-label text-muted ">Password</label>
-                        <input required type="password" name="password" value={this.state.password} onChange={this.changePassword} className="form-control" id="password" placeholder="Password" />
+                        <label for="trailer" className="form-label text-muted ">Trailer</label>
+                        <input required type="text" name="trailer" onChange={this.changeTrailer} value={this.state.trailer} className="form-control" id="trailer" placeholder="Movie Trailer" />
                     </div>
-
-                    <input type="submit" value="Sign Up" className="btn btn-outline-primary" />
+                    <div className="mb-3 text-start">
+                        <label for="summary" className="form-label text-muted ">Summary</label>
+                        <input required type="text" name="summary" onChange={this.changeSummary} value={this.state.summary} className="form-control" id="summary" placeholder="Movie Summary" />
+                    </div>
+                    <div className="mb-3 text-start">
+                        <label for="cast" className="form-label text-muted ">Cast</label>
+                        <input required type="cast" name="summary" onChange={this.changeCast} value={this.state.cast} className="form-control" id="cast" placeholder="Movie Cast" />
+                    </div>
+                    <div className="mb-3 text-start">
+                        <label for="language" className="form-label text-muted ">Language</label>
+                        <input required type="text" name="language" onChange={this.changeLanguage} value={this.state.language} className="form-control" id="language" placeholder="Movie Language" />
+                    </div>
+                    <div className="mb-3 text-start">
+                        <label for="genre" className="form-label text-muted ">Genre</label>
+                        <input required type="text" name="genre" onChange={this.changeGenre} value={this.state.genre} className="form-control" id="genre" placeholder="Movie Genre" />
+                    </div>
+                    <div className="mb-3 text-start">
+                        <label for="duration" className="form-label text-muted ">Duration</label>
+                        <input required type="text" name="duration" onChange={this.changeDuration} value={this.state.duration} className="form-control" id="duration" placeholder="Movie Duration" />
+                    </div>
+                    <div className="mb-3 text-start">
+                        <label for="censor board rating" className="form-label text-muted ">Censor Board Rating</label>
+                        <input required type="text" name="censor board rating" onChange={this.changeCensorBoardRating} value={this.state.censorBoardRating} className="form-control" id="censor board rating" placeholder="Movie Censor Board Rating" />
+                    </div>
+                    <div className="mb-3 text-start">
+                        <label for="in Theatres" className="form-label text-muted ">In Theatres</label>
+                        <input required type="text" name="in Theatres" onChange={this.changeInTheatres} value={this.state.inTheatres} className="form-control" id="in theatres" placeholder="Movie In Theatres" />
+                    </div>
+                    <div className="mb-3 text-start">
+                        <label for="BookMyShow link" className="form-label text-muted ">BookMyShow Link</label>
+                        <input required type="text" name="BookMyShowLink" onChange={this.changeBookMyShow} value={this.state.BookMyShow} className="form-control" id="BookMyShow Link" placeholder="Movie BookMyShow Link" />
+                    </div>
+                    <div className="mb-3 text-start">
+                        <label for="release date" className="form-label text-muted ">Release Date</label>
+                        <input required type="text" name="release date" onChange={this.changeReleaseDate} value={this.state.releaseDate} className="form-control" id="release date" placeholder="Movie Release Date" />
+                    </div>
+                    <div className="mb-3 text-start">
+                        <label for="platforms" className="form-label text-muted ">Platforms</label>
+                        <input required type="text" name="platforms" onChange={this.changePlatforms} value={this.state.platforms} className="form-control" id="platforms" placeholder="Movie Platforms" />
+                    </div>
+                    <div className="mb-3 text-start">
+                        <label for="category" className="form-label text-muted ">Category</label>
+                        <input required type="text" name="category" onChange={this.changeCategory} value={this.state.category} className="form-control" id="category" placeholder="Movie Category" />
+                    </div>
+                    <div className="mb-3 text-start">
+                        <label for="rating" className="form-label text-muted ">Rating</label>
+                        <input required type="text" name="rating" onChange={this.changeRating} value={this.state.rating} className="form-control" id="rating" placeholder="Movie Rating" />
+                    </div>
+                    
+                    <input type="submit" value="Add Movie" className="btn btn-outline-primary" />
                 </form>
-                <span className="text-start">Already a user? <a href="/login">Login here</a></span>
             </div>
         );
     }
 }
 
-export default SignupForm;
+export default AddMovieForm;
