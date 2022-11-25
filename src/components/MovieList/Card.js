419bfb0bd6043da5773a099c94cc32f0faad74c2
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2022-11-25 23:53:56 +0530

    load movie data onto frontend

--- src/components/MovieList/Card.js
+++ src/components/MovieList/Card.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import '../../index.css';
 import "./MovieList.css"
@@ -9,13 +8,13 @@ function Card(props) {
     return (
         <div className="MyCard">
             <div className="card" style={{ width: "18rem" }}>
-                <a href="/movie/123" className="">
+                <a href={props.link} className="">
                     <img src={props.imageUrl} className="CardImg card-img-top" alt="img" />
                 </a>
                 <div className="card-body">
                     <h5 className="card-title">{props.title}</h5>
                     <p className="card-text">{props.metades}</p>
-                    <a href="/movie/123" className="Button btn btn-primary">Review Movie</a>
+                    <a href={props.link} className="Button btn btn-primary">Review Movie</a>
                 </div>
             </div>
         </div>
--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -3,28 +3,55 @@ import Card from "./Card";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import '../../index.css';
 import "./MovieList.css"
+import axios from "axios";
 
+class MovieList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            movie: []
+        }
+    }
+    componentDidMount() {
+        axios.get("http://localhost:4000/movie/movies")
+            .then((res) => {
+                alert(res.data[0].title)
+                this.setState({ movie: (res.data) }, () => {
+                    alert(this.state.movie);
+                });
+            })
 
-function MovieList(){
-    return (
-        // movie list should be able to return the dynamic list of elements we want from a particular category
-        // use map to parse the whole Cards from database
-        <div className="container-fluid">
-        <Card imageUrl="https://images.pexels.com/photos/14270535/pexels-photo-14270535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
-        title="Inception" metades="Some quick example text to build on the card title and make up the bulk of the card's content." 
-        link="#" />
-        <Card imageUrl="https://images.pexels.com/photos/14270535/pexels-photo-14270535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
-        title="Inception" metades="Some quick example text to build on the card title and make up the bulk of the card's content." 
-        link="#" />
-        <Card imageUrl="https://images.pexels.com/photos/14270535/pexels-photo-14270535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
-        title="Inception" metades="Some quick example text to build on the card title and make up the bulk of the card's content." 
-        link="#" />
-        <Card imageUrl="https://images.pexels.com/photos/14270535/pexels-photo-14270535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
-        title="Inception" metades="Some quick example text to build on the card title and make up the bulk of the card's content." 
-        link="#" />
-        </div>
+    }
+    render() {
+        return (
+            // movie list should be able to return the dynamic list of elements we want from a particular category
+            // use map to parse the whole Cards from database
+            <div className="container-fluid">
+                <ul>
+                    {
+                        this.state.movie.map((movie) => {
+                            return (
+                                <>
+                                    <li>
+                                        <Card id={movie._id} imageUrl={movie.image}
+                                            title={movie.title} metades={movie.metadescription}
+                                            link={"http://localhost:3000/movie/"+movie._id} />
+                                    </li>
+                                </>
+                            );
+                        })
+                    }
+                </ul>
+                {/* <Card imageUrl="https://images.pexels.com/photos/14270535/pexels-photo-14270535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                    title="Inception" metades="Some quick example text to build on the card title and make up the bulk of the card's content."
+                    link="#" /> */}
 
-    );
+            </div>
+
+        );
+
+    }
+
 }
 
 export default MovieList;
