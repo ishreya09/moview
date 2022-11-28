281984d1098bc4d31897d7175c5c3e68b3d6ed17
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2022-11-28 12:38:58 +0530

    Category backend added

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -6,9 +6,28 @@ const MovieModels = require('../models/MovieModels');
 
 
 router.get("/:category",(request,response)=>{
-    MovieModels.find({category:{name:request.params.category}})
+    MovieModels.find()
     .then((movies)=>{
-        response.json(movies)
+        var category=[];
+        for(var i=0; i<movies.length;i++){
+            // console.log(movies[i].category)
+            for (var j=0;j<movies[i].category.length;j++){
+                if(movies[i].category[j].name == request.params.category){
+                    console.log(movies[i].title)
+                    // console.log(movies[i].category[j].name)
+                    category.push({
+                        _id: movies[i]._id,
+                        title: movies[i].title,
+                        image: movies[i].image,
+                        metadescription: movies[i].metadescription,
+                        rating:movies[i].rating,
+                    })
+
+                }
+            }
+        }
+        console.log (category)
+        response.json(category)
     })
     .catch((err)=>{
         response.json(err)
