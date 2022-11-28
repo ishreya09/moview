6febe052d245ebc1a0af1f89048e7f7e2017b748
Shreeja R <shreejar197@gmail.com>
2022-11-28 12:14:54 +0530

    contact us page edit

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -54,12 +54,12 @@ class ContactUs extends React.Component {
             <div className="ContactUsForm container-fluid">
                 <h1>Contact Us</h1>
                 <p className="text-start">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores autem cupiditate placeat assumenda quaerat, ut eveniet expedita saepe voluptatem illum cum optio ipsum enim illo, consequatur porro omnis est repudiandae sed sint minima ratione alias? Accusamus quisquam tempora repellat excepturi! Natus dolorum porro eveniet fugiat, odit quis in eaque, rem libero voluptatem quas blanditiis ipsam voluptatum, officiis sit praesentium rerum! Quasi magnam corrupti laborum pariatur dignissimos illum excepturi. A, suscipit. Ab cupiditate ducimus quo ex, perspiciatis corporis quidem repellendus laudantium itaque cum quisquam nihil magnam necessitatibus illum. Eius, ad est quam sed, non nesciunt error delectus vitae porro quisquam doloremque.
+                    Don't have your favourite movie ?? Contact Us .... Don't contact us for stupid stuff .. see we're PES Students
                 </p>
                 <form method="post" onSubmit={this.onContactSubmit}>
                     <div className="mb-3 text-start">
                         <label for="name" className="form-label text-muted ">Your Name</label>
-                        <input required type="text" name="name" className="form-control" onChange={this.changeName} id="name" placeholder="John Doe" />
+                        <input required type="text" name="name" className="form-control" onChange={this.changeName} id="name" placeholder="Your name" />
                     </div>
                     <div className="mb-3 text-start">
                         <label for="email" className="form-label text-muted ">Email address</label>
--- src/components/ContactUs/ThankYou.js
+++ src/components/ContactUs/ThankYou.js
@@ -3,7 +3,7 @@ import React from "react";
 function ThankYou(){
     return (
         <div className="container-fluid">
-            <h1 style={{position:"relative",padding:"100px"}} className="text-primary text-center">Thank You For Contacting Us. You will be contacted shortly</h1>
+            <h1 style={{position:"relative",padding:"100px"}} className="text-primary text-center">Thank You For Contacting Us. You will be contacted at some point of time </h1>
             <a style={{position:"relative"}}  href="/" className="Button text-center btn btn-primary">Go to Home</a>
         </div>
     );
